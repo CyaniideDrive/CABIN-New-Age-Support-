3cd4e7da941da0250b0c1d87a5675138f6119bd7
Danny <daniellboivin@gmail.com>
2025-09-07 01:50:40 -0400

    Update newage.js

--- kubejs/startup_scripts/startup_compatability/newage.js
+++ kubejs/startup_scripts/startup_compatability/newage.js
@@ -2,11 +2,11 @@
 if (Platform.isLoaded("create_new_age")) {
     // Sequence assembly items
     StartupEvents.registry("item", e => {
-        e.create("incomplete_electrical_connector", "create:sequenced_assembly").texture("create_new_age:electrical_connector").displayName("Incomplete Electrical Connector")
-	e.create("incomplete_redstone_magnet", "create:sequenced_assembly").texture("create_new_age:magnetite_block").displayName("Incomplete Redstone Magnet")
-	e.create("incomplete_layered_magnet", "create:sequenced_assembly").texture("create_new_age:redstone_magnet").displayName("Incomplete Layered Magnet")
-	e.create("incomplete_fluxuated_magnetite", "create:sequenced_assembly").texture("create_new_age:layered_magnet").displayName("Incomplete Fluxuated Magnetite")
-	e.create("incomplete_netherite_magnet", "create:sequenced_assembly").texture("create_new_age:fluxuated_magnetite").displayName("Incomplete Netherite Magnet")
-	e.create("incomplete_reactor_rod", "create:sequenced_assembly").texture("create_new_age:reactor_rod").displayName("Incomplete Reactor Rod")
+        e.create("incomplete_electrical_connector", "create:sequenced_assembly").texture("create_new_age:item/electrical_connector").displayName("Incomplete Electrical Connector").modelJson({parent: 'create_new_age:item/electrical_connector'})
+	e.create("incomplete_redstone_magnet", "create:sequenced_assembly").texture("create_new_age:item/magnetite_block").displayName("Incomplete Redstone Magnet").modelJson({parent: 'create_new_age:item/magnetite_block'})
+	e.create("incomplete_layered_magnet", "create:sequenced_assembly").texture("create_new_age:item/redstone_magnet").displayName("Incomplete Layered Magnet").modelJson({parent: 'create_new_age:item/redstone_magnet'})
+	e.create("incomplete_fluxuated_magnetite", "create:sequenced_assembly").texture("create_new_age:item/layered_magnet").displayName("Incomplete Fluxuated Magnetite").modelJson({parent: 'create_new_age:item/layered_magnet'})
+	e.create("incomplete_netherite_magnet", "create:sequenced_assembly").texture("create_new_age:item/fluxuated_magnetite").displayName("Incomplete Netherite Magnet").modelJson({parent: 'create_new_age:item/fluxuated_magnetite'})
+	e.create("incomplete_reactor_rod", "create:sequenced_assembly").texture("create_new_age:item/reactor_rod").displayName("Incomplete Reactor Rod").modelJson({parent: 'create_new_age:item/reactor_rod'})
     })
 }
